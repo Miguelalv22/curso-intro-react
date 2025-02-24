b2fd53187b975bca89169d646a21e91eb416585e
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-02-24 11:46:17 -0600

    Se logran cambiar los valores de los Todos totales y completed por medio de estados y props

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el Curso de Intro a React.js', completed: false },
   { text: 'Llorar con la llorona', completed: false },
-  { text: 'Probar deck de Slowking', completed: false }
+  { text: 'Probar deck de Slowking', completed: false },
+  { text: 'Usar estados derivados', completed: true }
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(
+    todo => !!todo.completed
+  ).length;
+
+  const totalTodos = todos.length;
+
   return (
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos} />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
