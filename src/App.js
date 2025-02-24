ea4367b321bf1fbb9e1f7869942caa622462dab4
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-02-24 12:42:44 -0600

    Se le da funcion al filtro del componente TodoSearch

--- src/App.js
+++ src/App.js
@@ -20,8 +20,16 @@ function App() {
   const completedTodos = todos.filter(
     todo => !!todo.completed
   ).length;
-
   const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      // return todo.text.toLowerCase().includes(searchValue.toLowerCase());
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLocaleLowerCase();
+      return todoText.includes(searchText);
+    }
+  );
 
   return (
     <>
@@ -32,7 +40,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
