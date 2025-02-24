959470624eca2e6630ec3ffd29b0891cad474dc7
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-02-24 17:28:52 -0600

    Se utiliza operador ternario para mostrar un mensaje de felicitaciones cuando no haya todos pendientes

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -2,9 +2,12 @@ import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
     return (
-        <h1 className='TodoCounter'>
-            Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-        </h1>
+        total == completed ?
+            <h1 className='TodoCounter'>Felicidades, no tienes TODOs pendientes</h1>
+            :
+            <h1 className='TodoCounter'>
+                Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+            </h1>
     );
 }
 
