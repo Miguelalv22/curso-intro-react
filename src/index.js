aca1ed19d4d024b550feaa884496870c7f13a091
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-03-04 12:06:09 -0600

    Se cambia la version a la 17.0.2 y se modifican archivos para que trabaje con esa misma

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+import { render } from 'react-dom';
+const root = document.getElementById('root');
+render(<App />, root);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
