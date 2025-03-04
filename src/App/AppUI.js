225b7d011694c660add0e1dc12c8390eb9b81e8d
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-03-03 18:29:54 -0600

    Se crea una ventana emergente para anadir un nuevo TODO y se le da funcionalidad tanto al boton de nuevo TODO como a la ventana para almacenarlo

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -6,8 +7,9 @@ import { TodosLoading } from '../TodosLoading';
 import { TodosError } from '../TodosError';
 import { EmptyTodos } from '../EmptyTodos';
 import { CreateTodoButton } from '../CreateTodoButton';
+import { TodoForm } from '../TodoForm';
+import { Modal } from '../Modal';
 import { TodoContext } from '../TodoContext';
-import React from 'react';
 
 function AppUI() {
     const {
@@ -15,41 +17,42 @@ function AppUI() {
         error,
         searchedTodos,
         completeTodo,
-        deleteTodo
+        deleteTodo,
+        openModal,
+        setOpenModal
     } = React.useContext(TodoContext);
 
     return (
         <>
             <TodoCounter />
             <TodoSearch />
+            <TodoList>
+                {loading && (
+                    <>
+                        <TodosLoading />
+                        <TodosLoading />
+                        <TodosLoading />
+                    </>
+                )}
+                {error && <TodosError />}
+                {(!loading && searchedTodos.length == 0) && <EmptyTodos />}
 
-            <TodoContext.Consumer>
-
-                <TodoList>
-                    {loading && (
-                        <>
-                            <TodosLoading />
-                            <TodosLoading />
-                            <TodosLoading />
-                        </>
-                    )}
-                    {error && <TodosError />}
-                    {(!loading && searchedTodos.length == 0) && <EmptyTodos />}
-
-                    {searchedTodos.map(todo => (
-                        <TodoItem
-                            key={todo.text}
-                            text={todo.text}
-                            completed={todo.completed}
-                            onComplete={() => completeTodo(todo.text)}
-                            onDelete={() => deleteTodo(todo.text)}
-                        />
-                    ))}
-                </TodoList>
-
-            </TodoContext.Consumer>
-
-            <CreateTodoButton />
+                {searchedTodos.map(todo => (
+                    <TodoItem
+                        key={todo.text}
+                        text={todo.text}
+                        completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
+                ))}
+            </TodoList>
+            <CreateTodoButton setOpenModal={setOpenModal} />
+            {openModal && (
+                <Modal>
+                    <TodoForm />
+                </Modal>
+            )}
         </>
     );
 };
